8bab359dc36da9cc0eed93e26633e01243513d04
seike <seike.112@icloud.com>
2019-10-07 12:42:47 +0000

    _error_messagesを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,23 @@ class UsersController < ApplicationController
   end
   
   def new
+     # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 end
